b3303c6024487cae818c077d409fa0d505354d32
Paul Le Blanc <91918397+P33TheWhite@users.noreply.github.com>
2024-03-21 09:09:51 +0100

    Update multiplication.php

--- php/multiplication.php
+++ php/multiplication.php
@@ -35,8 +35,45 @@
         }
     }
 
-    jeu();
+
+
+function revision1($table) {
+    $win = 0;
+    while ($win != 10) {
+        $rand = rand(0, 10);
+        echo ("$table x $rand = ?");
+        // Affiche un formulaire pour obtenir la réponse de l'utilisateur
+        echo "<form method='post'>";
+        echo "<input type='number' name='reponse'>";
+        echo "<input type='submit' value='Valider'>";
+        echo "</form>";
+        
+        if(isset($_POST['reponse'])) {
+            $result = intval($_POST['reponse']);
+            $true = $table * $rand;
+            if ($result == $true) {
+                $win++;
+            }
+        }
+    }
+}
+
+function jeu1() {
+    if (isset($_POST['table'])) {
+        $table = intval($_POST['table']);
+        revision1($table);
+    } else {
+        echo "<form method='post'>";
+        echo "Quelle table voulez-vous réviser ?";
+        echo "<input type='number' name='table'>";
+        echo "<input type='submit' value='Commencer'>";
+        echo "</form>";
+    }
+}
+
+jeu1();
 ?>
+
 
 </body>
 </html>
