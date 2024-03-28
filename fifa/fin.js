9477684dd93df5917d7b1171c8131ba4e7e9fbb2
Paul Le Blanc <91918397+P33TheWhite@users.noreply.github.com>
2024-03-28 08:36:13 +0100

    update 404

--- fifa/fin.js
+++ fifa/fin.js
@@ -1,11 +1,11 @@
 //prend le contenu passé en tant que paramètre 'content' dans l'URL puis injecte dans la page .right
 function formulaire(){
     //extraction des paramètres de la requête GET
-    let extraction = new URLSearchParams(window.location.search);
-    let contenu = decodeURIComponent(extraction.get('content'));
+    var extraction = new URLSearchParams(window.location.search);
+    var contenu = decodeURIComponent(extraction.get('content'));
 
     //cherche element de .right et charge son contenu
-    let rightElement = document.querySelector('.right');
+    var rightElement = document.querySelector('.right');
     if (rightElement) {
         rightElement.innerHTML = contenu;
     }
