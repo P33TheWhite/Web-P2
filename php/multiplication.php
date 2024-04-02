20ccdbbe4f84b09e97f396c241b3cd2efb467bff
Paul Le Blanc <91918397+P33TheWhite@users.noreply.github.com>
2024-04-02 10:53:01 +0200

    Update multiplication.php

--- php/multiplication.php
+++ php/multiplication.php
@@ -1,43 +1,14 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" lang="fr">
-        <title>Table de multiplication</title>
-    </head>
+<head>
+    <meta charset="utf-8" lang="fr">
+    <title>Table de multiplication</title>
+</head>
 <body>
 
 <?php
 
-    function revision($table){
-        $win=0;
-        while($win!=10){
-            $rand=rand(0,10);
-            echo ("$table x $rand = ?");
-            $result = fgets(STDIN);
-            $true=$table*$rand;
-            if($result== $true){
-                $win++;
-            }
-        }
-    }
-
-    function jeu(){
-        echo "Voulez-vous reviser une table aléatoire ?";
-        $rep = fgets(STDIN);
-        if($rep == "oui"){
-            $tablerandom=rand(1,10);
-            revision($tablerandom);
-        }
-        else{
-            echo "Quelle table voulez vous réviser ?";
-            $table = fgets(STDIN);
-            revision($table);
-        }
-    }
-
-
-
-function revision1($table) {
+function revision($table) {
     $win = 0;
     while ($win != 10) {
         $rand = rand(0, 10);
@@ -46,9 +17,8 @@ function revision1($table) {
         echo "<form method='post'>";
         echo "<input type='number' name='reponse'>";
         echo "<input type='submit' value='Valider'>";
-
         echo "</form>";
-        
+
         if(isset($_POST['reponse'])) {
             $result = intval($_POST['reponse']);
             $true = $table * $rand;
@@ -59,22 +29,29 @@ function revision1($table) {
     }
 }
 
-function jeu1() {
-    if (isset($_POST['table'])) {
-        $table = intval($_POST['table']);
-        revision1($table);
-    } else {
-        echo "<form method='post'>";
-        echo "Quelle table voulez-vous réviser ?";
-        echo "<input type='number' name='table'>";
-        echo "<input type='submit' value='Commencer'>";
-        echo "</form>";
+function jeu() {
+    echo "<form method='post'>";
+    echo "Voulez-vous réviser une table aléatoire ? (oui/non)";
+    echo "<input type='text' name='tablealea'>";
+    echo "<input type='submit' value='Commencer'>";
+    echo "</form>";
+    if(isset($_POST['tablealea'])){
+        if ( $_POST['tablealea'] == "oui") {
+            $tablerandom = rand(1, 10);
+            revision($tablerandom);
+        }
+        else{
+            echo "<form method='post'>";
+            echo "Quelle table voulez-vous réviser ?";
+            echo "<input type='number' name='table'>";
+            echo "<input type='submit' value='Commencer'>";
+            echo "</form>";
+        }
     }
 }
 
-jeu1();
+jeu();
 ?>
-
 
 </body>
 </html>
